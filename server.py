01d5cd97ceeb5a52d6b84d09578ebde37074523e
Carlos Caicedo <calicheccmusica@gmail.com>
2022-01-24 13:10:55 -0800

    initial commit

---
+++ server.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, session, redirect,request
+
+app = Flask(__name__)
+
+app.secret_key="!Loveb0o$$$$$735"
+
+@app.route('/')
+def index():
+    return render_template("survey.html")
+
+
+@app.route('/intake', methods=['POST'])
+def process():
+    session['name'] = request.form['name']
+    session['location'] = request.form['location']
+    session['language'] = request.form['language']
+    session['comments'] = request.form['comments']
+    return redirect('/received')
+
+@app.route('/received')
+def success():
+    return render_template('display_info.html')
+    
+if __name__=="__main__":
+    app.run(debug=True)
